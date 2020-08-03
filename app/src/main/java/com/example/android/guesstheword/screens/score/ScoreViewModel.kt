29bf9f8d7e56bfafe8565a41710227993b940e3c
PARINYA <parinya19948@gmail.com>
2020-08-03 16:25:29 +0700

    05.2: LiveData and LiveData observers 9. Task: Add the Play Again button

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -10,10 +10,19 @@ class ScoreViewModel(finalScore: Int) : ViewModel() {
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
     init {
-        Log.i("ScoreViewModel", "Final score is $finalScore")
         _score.value = finalScore
     }
-
-
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
 }
+
+
+
