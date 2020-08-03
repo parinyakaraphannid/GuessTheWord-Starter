d96b93cdfa968c8b8e7592ec4a3025f4e24bf226
PARINYA <parinya19948@gmail.com>
2020-08-03 16:13:38 +0700

    05.2: LiveData and LiveData observers 8. Task: Add LiveData to the ScoreViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -39,6 +40,7 @@ class ScoreFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
+
         // Inflate view and obtain an instance of the binding class.
         val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
                 inflater,
@@ -46,10 +48,13 @@ class ScoreFragment : Fragment() {
                 container,
                 false
         )
-        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
-        viewModel = ViewModelProviders.of(this, viewModelFactory)
+         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
-        binding.scoreText.text = viewModel.score.toString()
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        
         return binding.root
     }
 }
